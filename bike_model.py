d77829cf001bc92d517f3ba79154eab083025595
Joe Milbourn <joe@milbourn.org.uk>
2012-10-05 18:37:06 +0100

    Added inital model

---
+++ bike_model.py
@@ -0,0 +1,56 @@
+from math import sin
+from numpy import deg2rad, mean, diff, abs
+from collections import namedtuple
+from pylab import plot, legend, xlabel, ylabel, show
+
+class Chap:
+    def __init__ (self, name, mass, width, height, initial_v=0):
+        self.mass = mass
+        self.height = height
+        self.width = width
+        self.v = [initial_v]
+        self.name = name
+
+    def __str__ (self):
+        return self.name
+
+##People
+chaps = [Chap("David", 85.7, 0.7, 2),
+         Chap("Joe", 90, 0.65, 2), 
+         Chap("James", 120, 1, 2)]
+
+## Constants
+tor = 0.1 #s
+slope = deg2rad(10) #degrees
+G = 9.81
+Cd = 1
+rho = 1.2
+rolling_R = 0.003
+
+def drag (chap):
+    air_drag = 0.5*rho * Cd * chap.v[-1]**2 * chap.width*chap.height
+    rolling_drag = rolling_R * chap.mass * G
+    return air_drag + rolling_drag
+
+def force (chap):
+        return chap.mass * G * sin(slope)
+
+def steady_state (chap, npts=10, eps=0.0001):
+    if len(chap.v) < npts: return False
+    else: return mean(abs(diff(chap.v[-npts:]))) < eps
+
+for iteration in xrange(10000):
+    for chap in chaps:
+        chap.v += [chap.v[-1] + tor * ((force(chap) - drag(chap))/chap.mass)]
+    if all(steady_state(chap) for chap in chaps):
+    	break
+
+t = [tor * i for i in range(len(chaps[0].v))]
+
+for chap in chaps:
+	plot(t, chap.v, label=str(chap))
+
+legend(loc='best')
+xlabel('Time (s)')
+ylabel('Speed (m/s)')
+show()
